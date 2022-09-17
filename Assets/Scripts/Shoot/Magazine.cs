d4aabb8d63d265353d1af0f11eda2e4a6c655028
CyanMisaki <fluffyshy@yandex.ru>
2022-09-17 15:32:58 +0300

    5

---
+++ Assets/Scripts/Shoot/Magazine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Shoot
+{
+    public struct WeaponType
+    {
+        public enum magazType
+        {
+            pistol,
+            rifle
+        };
+    }
+    public class Magazine: MonoBehaviour
+    {
+        public WeaponType.magazType typeOfMagazine = WeaponType.magazType.pistol;
+        public int ammo = 30;
+
+        [HideInInspector] public bool isOpen = true;
+        [HideInInspector] public Rigidbody myRig;
+        [HideInInspector] public Collider myCol;
+        [HideInInspector] public GameObject Bullet;
+
+
+        private void Start()
+        {
+            myRig = GetComponent<Rigidbody>();
+            myCol = GetComponent<Collider>();
+
+            if (transform.GetChild(0)) Bullet = transform.GetChild(0).gameObject;
+
+        }
+    }
+}
